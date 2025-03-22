053221f5cf09950442d579db8e2d63e3769495da
FIC <fic@info.local>
2025-03-21 22:00:41 -0300

    Atualizando Projeto Revisão

--- ProjetoRevisao/Program.cs
+++ ProjetoRevisao/Program.cs
@@ -5,28 +5,83 @@
 //2. Exibir as notas e calcular a média.
 //3. Verificar se o aluno foi aprovado ou reprovado (média >= 7 para aprovação).
 
-int[] notas = new int[5];
-int somaNotas = 0, qtdNotas = 0;
+double[] notas = new double[5];
+double somaNotas = 0, qtdNotas = 0, media = 0;
+int menu = 0;
 
-for (int i = 0; i < notas.Length; i++)
+while (menu != 4)
 {
-
-    do
+    Console.WriteLine("=== Gerenciador de Notas ===");
+    Console.WriteLine("1. Adicionar aluno");
+    if (qtdNotas != 0)
     {
-        Console.WriteLine("Informe uma nota: ");
-        notas[i] = int.Parse(Console.ReadLine());
+        Console.WriteLine("2. Listar alunos");
+        Console.WriteLine("3. Estatísticas");
+    }
+    Console.WriteLine("4. Sair");
+    Console.Write("Escolha uma opção: ");
+    menu = int.Parse(Console.ReadLine());
 
-        if (notas[i] >= 0 && notas[i] <= 10)
+    if (menu == 1)
+    {
+        try
         {
-            somaNotas = somaNotas + notas[i];
-            qtdNotas++;
-        } else
+            for (int i = 0; i < notas.Length; i++)
+            {
+
+                do
+                {
+                    Console.WriteLine($"Digite a {i + 1}ª nota: ");
+                    notas[i] = double.Parse(Console.ReadLine());
+
+                    if (notas[i] >= 0 && notas[i] <= 10)
+                    {
+                        somaNotas = somaNotas + notas[i];
+                        qtdNotas++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Digite uma nota entre 0 e 10");
+                    }
+
+                } while (notas[i] < 0 || notas[i] > 10);
+            }
+        }
+        catch (FormatException ex)
         {
-            Console.WriteLine("Digite uma nota entre 0 e 10");
+            Console.WriteLine("Erro: Digite somente número!");
+        }
+    }
+    else if (menu == 2)
+    {
+        Console.WriteLine("Você escolheu a opção 2.");
+    }
+    else if (menu == 3)
+    {
+        Console.Clear();
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}ª nota: {notas[i]}");
         }
 
-    } while (notas[i] < 0 || notas[i] > 10);
+        if (media >= 7.40)
+        {
+            Console.WriteLine($"A médias das notas é: {media}");
+            Console.WriteLine("O aluno foi APROVADO! :)");
+        }
+        else
+        {
+            Console.WriteLine($"A médias das notas é: {media}");
+            Console.WriteLine("O aluno foi REPROVADO!");
+        }
+        Thread.Sleep(10000);
+    }
+    else if (menu == 4)
+    {
+        Console.WriteLine("Você saiu do menu.");
+    }
+    else
+    {
+        Console.WriteLine("Escolha inválida, tente novamente.");
+    }
 }
-
-int media = somaNotas / qtdNotas;
-Console.WriteLine(media);
